becc3d42600d2db35e1916ef077793edd4a1a3e5
marimo <126547031+seniora-pink@users.noreply.github.com>
2023-08-15 15:11:20 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+
+const botToken = "6504257620:AAH4_RxmRPAWuIlQlpwwFirf6IziGssdKGQ";
+const apiUrl = "https://api.telegram.org/bot" + botToken + "/getUpdates";
+
+
 const DemoApp = {
         initData: Telegram.WebApp.initData || '',
         initDataUnsafe: Telegram.WebApp.initDataUnsafe || {},
         MainButton: Telegram.WebApp.MainButton,
 
         init(options) {
-            document.body.style.visibility = '';
             Telegram.WebApp.ready();
             Telegram.WebApp.MainButton.setParams({
                 text: 'CLOSE WEBVIEW',
@@ -15,140 +19,42 @@ const DemoApp = {
             Telegram.WebApp.close();
         },
 
-        scanQrCode(button) {
-            if (!DemoApp.initDataUnsafe.query_id) {
-                alert('WebViewQueryId not defined');
-                return;
-            }
+        scanQrCode() {
 
             document.querySelectorAll('button').forEach((btn) => btn.disabled = true);
 
-            const btn = document.querySelector('#btn_status');
-            btn.textContent = 'Sending...';
+            fetch(apiUrl)
+              .then(response => response.json())
+              .then(data => {
+                if (data.ok) {
+                  const updates = data.result;
+                  if (updates.length > 0) {
+                    const chatId = updates[updates.length - 1].chat.id;
+                    console.log("Newest update:", chatId);
+                  } else {
+                    console.log("No updates received.");
+                  }
+                } else {
+                  console.log("Request failed:", data.description);
+                }
+              })
+              .catch(error => {
+                console.error("Error:", error);
+              });
 
             Telegram.WebApp.showScanQrPopup({text: 'with any link'}, function (qrCode) {
                 // Process the scanned QR code for order
                 // For this example, we assume the QR code contains the order ID
-                if (button == "order") {
-                    DemoApp.apiRequest('/orders/' + qrCode, function(result) {
-                        document.querySelectorAll('button').forEach((btn) => btn.disabled = false);
+                Telegram.WebApp.sendMessage({
+                  chat_id: chatId,
+                  text: qrCode,
+                  token: botToken,
+                });
 
-                        if (result.response) {
-                            if (result.response.ok) {
-                                btn.textContent = 'Message sent successfully!';
-                                btn.className = 'ok';
-                                btn.style.display = 'block';
-                            } else {
-                                btn.textContent = result.response.description;
-                                btn.className = 'err';
-                                btn.style.display = 'block';
-                                alert(result.response.description);
-                            }
-                        } else if (result.error) {
-                            btn.textContent = result.error;
-                            btn.className = 'err';
-                            btn.style.display = 'block';
-                            alert(result.error);
-                        } else {
-                            btn.textContent = 'Unknown error';
-                            btn.className = 'err';
-                            btn.style.display = 'block';
-                            alert('Unknown error');
-                        }
-                    });
-                } else {
-                    DemoApp.apiRequest('/products/' + qrCode, function(result) {
-                        document.querySelectorAll('button').forEach((btn) => btn.disabled = false);
-
-                        if (result.response) {
-                            if (result.response.ok) {
-                                btn.textContent = 'Message sent successfully!';
-                                btn.className = 'ok';
-                                btn.style.display = 'block';
-                            } else {
-                                btn.textContent = result.response.description;
-                                btn.className = 'err';
-                                btn.style.display = 'block';
-                                alert(result.response.description);
-                            }
-                        } else if (result.error) {
-                            btn.textContent = result.error;
-                            btn.className = 'err';
-                            btn.style.display = 'block';
-                            alert(result.error);
-                        } else {
-                            btn.textContent = 'Unknown error';
-                            btn.className = 'err';
-                            btn.style.display = 'block';
-                            alert('Unknown error');
-                        }
-                    });
-                }
-                Telegram.WebApp.showAlert(qrCode);
+                DemoApp.close();
                 return true;
-            });
-        },
-        apiRequest(endpoint, onCallback) {
-            // DISABLE BACKEND FOR FRONTEND DEMO
-            // YOU CAN USE YOUR OWN REQUESTS TO YOUR OWN BACKEND
-            // CHANGE THIS CODE TO YOUR OWN
-//            return onCallback && onCallback({error: 'This function (' + method + ') should send requests to your backend. Please, change this code to your own.'});
-            const api_url = 'http://localhost:8000'
-
-            fetch(api_url + endpoint
-            ).then(function(response) {
-                return response.json();
-            }).then(function(result) {
-                onCallback && onCallback(result);
-            }).catch(function(error) {
-                onCallback && onCallback({error: 'Server error'});
             });
         }
 }
 
-
-function mainToProductBtn() {
-    document.getElementById("main_btn").style.display = "none";
-    document.getElementById("product_btn").style.display = "block";
-}
-
-postData = {
-                        "order_id": orderId,
-                        "product_id": qrCode,
-                        "product_name": productName,
-                        "date_of_arrival": 5545.322,
-                        "temporary_location": "AAA4512"
-                    };
-                    fetch(apiUrl + "/reports/arrival_date", {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiY29udHJvbGxlciIsImV4cGlyZXMiOjIzMjExNzc0ODIuMjI2MzAwMn0.X4VA_ST28ETT9KF6KMPEsnpdqDWlT8yUwNaQbcvi4-0'
-                        },
-                        body: JSON.stringify(postData)
-                    })
-                    .then(response => {
-                        // Check if the request was successful
-                        if (!response.ok) {
-                            throw new Error('Network response was not ok');
-                        }
-                        // Parse the response data as JSON
-                        return response.json();
-                    })
-                    .then(data => {
-                        // Process the data returned from the server
-                        console.log(data);
-                        // Disable only the selected product button
-                        const productButtons = document.getElementById('product_list').getElementsByTagName('button');
-                        for (let i = 0; i < productButtons.length; i++) {
-                            const button = productButtons[i];
-                            if (button.textContent === productName) {
-                                button.disabled = true;
-                                button.style.backgroundColor = 'gray'
-                            }
-                        }
-                    })
-                    .catch(error => {
-                        // Handle any errors that occurred during the fetch
-                        console.error('Error:', error);
-                    });
+DemoApp.init();
